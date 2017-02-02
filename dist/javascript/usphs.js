84f2122578ea58dddb9dacddc3a720401159a005
leechengpeng <leechengpeng@126.com>
2017-02-02 14:28:29 +0800

    fix usphs bug

--- dist/javascript/usphs.js
+++ dist/javascript/usphs.js
@@ -5,7 +5,11 @@ $(document).ready(function(){
 	// INIT PARAMENTERS
 	var UID = Number(requestParameter("uid")),
 		TID = Number(requestParameter("tid")),
-		CID = Number(requestParameter("cid"));
+		CID = Number(requestParameter("cid")),
+		IsEditMode = false;
+	// INIT SELECTOR
+	var $InfoSegement = $('table'),
+		$FormSegement = $('form');
 
 
 	// **************************************************
@@ -17,10 +21,15 @@ $(document).ready(function(){
 		dataType : "JSON",
 		error    : function() {
 			// network error
-
+			$FormSegement.show();
 		},
 		success  : function(vData) {
-			
+			$InfoSegement.show();
+
+			IsEditMode = true;
+			showData(vData);
+			setDefultFormData(vData);
+			$FormSegement.hide();
 		}
 	});
 	// **************************************************
@@ -39,12 +48,48 @@ $(document).ready(function(){
 
 
 	// **************************************************
+	// POST | PUT
+	$('form').form({
+		onSuccess: function(){
+			$.ajax({
+				url      : URL_USPHS,
+				type     : IsEditMode ? "PUT" : "POST", 
+				data     : toform({user_id : UID, case_id : CID, tooth_id : TID}) + $(this).serialize(),
+				dataType : "json",
+				error    : function() {networkError();},
+				success  : function() {location.reload();}
+			});
+			
+			return false;
+		}
+	});
+
+
+	// **************************************************
 	// Other Envent
 	$('.edit.button').click(function(){
-		window.location = "riskevaluation.html" + toquerystring({
-			uid  : UID,
-			tid  : TID,
-			cid  : CID,
-		});
+		$InfoSegement.hide();
+   		$FormSegement.find('.submit.button').text("确认修改").after('<div class="ui right floated teal small button" onclick="location.reload()">取消</div>');
+		$FormSegement.show();
 	});
+
+
+	// **************************************************
+	// Function
+	function showData(vData) {
+		$('#color').text(vData.color);
+		$('#marginal_accuracy').text(vData.marginal_accuracy);
+		$('#anatomic_form').text(vData.anatomic_form);
+		$('#edge_color').text(vData.edge_color);
+		$('#secondary_caries').text(vData.secondary_caries);
+		$('#usphs_level').text(vData.level);
+	}
+
+	function setDefultFormData(vData) {
+		$('input[value=' + vData.color + ']').parent().checkbox('check'); 
+		$('input[value=' + vData.marginal_accuracy + ']').parent().checkbox('check'); 
+		$('input[value=' + vData.anatomic_form + ']').parent().checkbox('check'); 
+		$('input[value=' + vData.edge_color + ']').parent().checkbox('check'); 
+		$('input[value=' + vData.secondary_caries + ']').parent().checkbox('check'); 
+	}
 });
