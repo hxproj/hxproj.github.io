6591311b0584706912329499937a78826c293da6
leechengpeng <leechengpeng@126.com>
2017-02-08 15:33:05 +0800

    show cure data

--- dist/javascript/cure.js
+++ dist/javascript/cure.js
@@ -336,18 +336,76 @@ $(document).ready(function(){
 			};
 			// 龋病微创修复
 			case 1: {
+				DescriptionStr = "<bold>" + vData.specific_method + "：</bold>" + NewLine;
+				switch (vData.specific_method) {
+					case "ART修复": {
+						DescriptionStr = showMinimal1(vData, DescriptionStr);
+						break;
+					}
+					case "预防性充填": {
+						DescriptionStr = showMinimal2(vData, DescriptionStr);
+						break;
+					}
+					case "玻璃离子过渡性修复": {
+						DescriptionStr = showMinimal3(vData, DescriptionStr);
+						break;
+					}
+					case "釉质成型术": {
+						DescriptionStr = showMinimal4(vData, DescriptionStr);
+						break;
+					}
+					case "微打磨术": {
+						DescriptionStr = showMinimal5(vData, DescriptionStr);
+						break;
+					}
+					default: {
+						showErrorInfo();
+					}
+				}
+
 				break;
 			};
 			// 复合树脂修复
 			case 2: {
+				DescriptionStr = "<bold>" + vData.specific_method + "：</bold>" + NewLine;
+				switch (vData.specific_method) {
+					case "牙树脂直接充填修复": {
+						DescriptionStr = showResin1(vData, DescriptionStr);
+						break;
+					}
+					case "牙安抚治疗&树脂充填修复": {
+						DescriptionStr = showResin2(vData, DescriptionStr);
+						break;
+					}
+					default: {
+						showErrorInfo();
+					}
+				}
+
 				break;
 			};
 			// 美容修复
 			case 3: {
+				DescriptionStr = "<bold>" + vData.specific_method + "：</bold>" + NewLine;
+				switch (vData.specific_method) {
+					case "渗透树脂修复": {
+						DescriptionStr = showLook1(vData, DescriptionStr);
+						break;
+					}
+					case "微创复合树脂分层修复": {
+						DescriptionStr = showLook2(vData, DescriptionStr);
+						break;
+					}
+					default: {
+						showErrorInfo();
+					}
+				}
+				
 				break;
 			};
 			// 间接修复
 			case 4: {
+				DescriptionStr = showIndirect(vData, DescriptionStr);
 				break;
 			};
 			default: {
@@ -367,14 +425,379 @@ $(document).ready(function(){
 		return vDescription;
 	}
 	function shownoSurgical3(vData, vDescription) {
-		vDescription  += "" + NewLine
-  							+ "1. 清洁牙面： 在低速手机上装好" + vData.additional_device 
-  							+ "，蘸取适量" + vData.reagent + "于牙面，对牙面和窝沟来回刷洗1分钟，同时不断滴水保持毛刷湿润" + NewLine
-  							+ "2. 用棉纱球隔湿，压缩空气牙面吹干，" + vData.tools + "蘸取酸蚀剂置于牙尖斜面的2/3上。" + vData.time_of_etching + NewLine
-  							+ "3. 流水冲洗牙面10-15秒，去除牙釉质表面和反应沉淀物" + NewLine
-  							+ "4. 洗刷笔蘸取适量封闭剂沿窝沟从远中向近中涂布在酸蚀后的牙面上" + NewLine
-  							+ "5. " + vData.lamp + NewLine
-  							+ "6. 探针进行检查，调合，" + vData.check_time;
+		vDescription  +=  "1. 清洁牙面： 在低速手机上装好" + vData.additional_device 
+						+ "，蘸取适量" + vData.reagent + "于牙面，对牙面和窝沟来回刷洗1分钟，同时不断滴水保持毛刷湿润" + NewLine
+						+ "2. 用棉纱球隔湿，压缩空气牙面吹干，" + vData.tools + "蘸取酸蚀剂置于牙尖斜面的2/3上。" + vData.time_of_etching + NewLine
+						+ "3. 流水冲洗牙面10-15秒，去除牙釉质表面和反应沉淀物" + NewLine
+						+ "4. 洗刷笔蘸取适量封闭剂沿窝沟从远中向近中涂布在酸蚀后的牙面上" + NewLine
+						+ "5. " + vData.lamp + NewLine
+						+ "6. 探针进行检查，调合，" + vData.check_time;
+		return vDescription;
+	}
+	// 龋病微创修复
+	function showMinimal1(vData, vDescription) {
+		vDescription += "1. ";
+		// FIXME: add tooth location
+		if (vData.microscope == "显微镜下") {
+			vDescription += vData.microscope + "，";
+		}
+		vDescription += vData.tools + "去龋，以龋蚀显示剂指示，继续去净龋坏，";
+		vDescription += vData.shape_of_hole + "制备洞形，深度：";
+		vDescription += vData.depth_of_hole + "mm" + NewLine;
+
+		vDescription += "2. 干燥，隔湿，";
+		vDescription += vData.is_piece + "，";
+		vDescription += vData.is_chock + NewLine;
+
+		vDescription += "3. 处理剂清洁窝洞，彻底冲洗，吹干" + NewLine;
+		vDescription += "4. 干燥隔湿，使用玻璃离子充填龋洞" + NewLine;
+		vDescription += "5. 修形，调合，涂布凡士林" + NewLine;
+
+		return vDescription;
+	}
+	function showMinimal2(vData, vDescription) {
+		vDescription += "1. ";
+		// FIXME: add tooth location
+		if (vData.microscope == "显微镜下") {
+			vDescription += vData.microscope + "，";
+		}
+		vDescription += vData.tools + "去龋，以龋蚀显示剂指示，继续去净龋坏，";
+		vDescription += vData.shape_of_hole + "制备洞形，深度：";
+		vDescription += vData.depth_of_hole + "mm" + NewLine;
+
+		vDescription += "2. 干燥，隔湿，";
+		vDescription += vData.is_piece + "，";
+		vDescription += vData.is_chock + NewLine;
+
+		vDescription += "3. ";
+		vDescription += vData.shade_guide + "比色，选择牙色";
+		vDescription += vData.color_of_tooth + NewLine;
+
+		vDescription += "4. ";
+		vDescription += vData.disinfect + "窝洞消毒";
+		if (vData.bottom) {
+			vDescription += "，" + vData.bottom;
+		};
+		vDescription += NewLine;
+
+		vDescription += "5. 酸蚀，彻底冲洗，吹干" + NewLine;
+
+		vDescription += "6. ";
+		vDescription += vData.etching_type + "：";
+		if (vData.etching_type == "全酸蚀粘接系统") {
+			vDescription += vData.full_etching + "，";
+		} else {
+			vDescription += vData.self_etching + "，";
+		}
+		vDescription += "涂布" + vData.coating_time + "，";
+		vDescription += "光照" + vData.illumination_time;
+		vDescription += NewLine;
+
+		vDescription += "7. ";
+		vDescription += "树脂：" + vData.resin + "，";
+		vDescription += "颜色：" + vData.color_of_resin;
+		vDescription += NewLine;
+
+		vDescription += "8. " + vData.modification + "，";
+		vDescription += vData.lamp + "光照" + vData.time_of_lamp;
+		vDescription += NewLine;
+
+		vDescription += "9. ";
+		vDescription += vData.modulo + "调合" + "，";
+		vDescription += vData.polishing + "抛光";
+
+		return vDescription;
+	}
+	function showMinimal3(vData, vDescription) {
+		vDescription += "1. ";
+		// FIXME: add tooth location
+		if (vData.anesthesia_medicine != "无") {
+			vDescription += vData.anesthesia_medicine + "，";
+		}
+		if (vData.part_anesthesia != "无") {
+			vDescription += vData.part_anesthesia + "，";
+		}
+		vDescription += vData.rubber;
+		vDescription += NewLine;
+
+		vDescription += "2. ";
+		// FIXME: add tooth location
+		if (vData.microscope == "显微镜下") {
+			vDescription += vData.microscope + "，";
+		}
+		vDescription += vData.tools + "去龋，以龋蚀显示剂指示，继续去净龋坏，";
+		vDescription += vData.shape_of_hole + "制备洞形，深度：";
+		vDescription += vData.depth_of_hole + "mm" + NewLine;
+
+		vDescription += "3. 干燥，隔湿，";
+		vDescription += vData.is_piece + "，";
+		vDescription += vData.is_chock + NewLine;
+
+		vDescription += "4. ";
+		vDescription += vData.disinfect + "窝洞消毒";
+		if (vData.bottom != "无") {
+			vDescription += "，" + vData.bottom;
+		}
+		vDescription += NewLine;
+
+		vDescription += "5. " + vData.modulo + "玻璃离子充填" + NewLine;
+		vDescription += "6. 修型，调合，涂凡士林";
+		
+		return vDescription;
+	}
+	function showMinimal4(vData, vDescription) {
+		vDescription += "用火焰状金刚砂针磨去浅的沟裂，将釉质磨圆钝，形成光滑、蝶形的利于清洁的表面";
+		
+		return vDescription;
+	}
+	function showMinimal5(vData, vDescription) {
+		vDescription += "1. 低速手机装上" + vData.microscope + "蘸取适量牙膏于牙面，来回刷洗无龋牙面及窝沟1min，彻底冲洗清洁" + NewLine;
+		vDescription += "2. 涂抹约2～3mm厚的磨膏层于牙面，低速手机装上" + vData.modulo
+			 + "蘸取6.6%盐酸及碳化硅微粒的水溶性磨砂膏剂来进行微打磨，轻微加力，在牙齿表面打磨(2-3min，4-6min)。在每次微打磨之后，使用清水冲洗干净";
+		
+		return vDescription;
+	}
+	// 复合树脂修复
+	function showResin1(vData, vDescription) {
+		vDescription += "1. ";
+		// FIXME: add tooth location
+		if (vData.anesthesia_medicine != "无") {
+			vDescription += vData.anesthesia_medicine + "，";
+		}
+		if (vData.part_anesthesia != "无") {
+			vDescription += vData.part_anesthesia + "，";
+		}
+		vDescription += vData.rubber;
+		vDescription += NewLine;
+
+		vDescription += "2. ";
+		// FIXME: add tooth location
+		if (vData.microscope == "显微镜下") {
+			vDescription += vData.microscope + "，";
+		}
+		vDescription += vData.tools + "去龋，以龋蚀显示剂指示，继续去净龋坏，";
+		vDescription += vData.shape_of_hole + "制备洞形，深度：";
+		vDescription += vData.depth_of_hole + "mm" + NewLine;
+
+		vDescription += "3. 干燥，隔湿，";
+		vDescription += vData.is_piece + "，";
+		vDescription += vData.is_chock + NewLine;
+
+		vDescription += "4. ";
+		vDescription += vData.shade_guide + "比色，选择牙色";
+		vDescription += vData.color_of_tooth + NewLine;
+
+		vDescription += "5. ";
+		vDescription += vData.disinfect + "窝洞消毒";
+		if (vData.bottom != "") {
+			vDescription += "，" + vData.bottom;
+		}
+		vDescription += NewLine;
+
+		vDescription += "6. ";
+		vDescription += vData.etching_type + "：";
+		if (vData.etching_type == "全酸蚀粘接系统") {
+			vDescription += vData.full_etching + "，";
+		} else {
+			vDescription += vData.self_etching + "，";
+		}
+		vDescription += "涂布" + vData.coating_time + "，";
+		vDescription += "光照" + vData.illumination_time;
+		vDescription += NewLine;
+
+		vDescription += "7. ";
+		vDescription += "树脂：" + vData.resin + "，";
+		vDescription += "颜色：" + vData.color_of_resin;
+		vDescription += NewLine;
+
+		vDescription += "8. " + vData.modification + "，";
+		vDescription += vData.lamp + "光照" + vData.time_of_lamp;
+		vDescription += NewLine;
+
+		vDescription += "9. ";
+		vDescription += vData.modulo + "调合" + "，";
+		vDescription += vData.polishing + "抛光";
+		
+		return vDescription;
+	}
+	function showResin2(vData, vDescription) {
+		// 初诊
+		vDescription += "<bold>初诊：</bold>";
+		vDescription += "使用"+ vData.appease_medicine + "，";
+		vDescription += "观察" + vData.observed_time;
+		vDescription += NewLine;
+
+		// 复诊：1. 
+		vDescription += "<bold>复诊：</bold>" + NewLine;
+		
+		vDescription += "1. ";
+		// FIXME: add tooth location
+		if (vData.anesthesia_medicine != "无") {
+			vDescription += vData.anesthesia_medicine + "，";
+		}
+		if (vData.part_anesthesia != "无") {
+			vDescription += vData.part_anesthesia + "，";
+		}
+		vDescription += vData.rubber;
+		vDescription += NewLine;
+
+		vDescription += "2. ";
+		// FIXME: add tooth location
+		if (vData.microscope == "显微镜下") {
+			vDescription += vData.microscope + "，";
+		}
+		vDescription += vData.tools + "去龋，以龋蚀显示剂指示，继续去净龋坏，";
+		vDescription += vData.shape_of_hole + "制备洞形，深度：";
+		vDescription += vData.depth_of_hole + "mm" + NewLine;
+
+		vDescription += "3. 干燥，隔湿，";
+		vDescription += vData.is_piece + "，";
+		vDescription += vData.is_chock + NewLine;
+
+		vDescription += "4. ";
+		vDescription += vData.shade_guide + "比色，选择牙色";
+		vDescription += vData.color_of_tooth + NewLine;
+
+		vDescription += "5. ";
+		vDescription += vData.disinfect + "窝洞消毒";
+		if (vData.bottom != "") {
+			vDescription += "，" + vData.bottom;
+		}
+		vDescription += NewLine;
+
+		vDescription += "6. ";
+		vDescription += vData.etching_type + "：";
+		if (vData.etching_type == "全酸蚀粘接系统") {
+			vDescription += vData.full_etching + "，";
+		} else {
+			vDescription += vData.self_etching + "，";
+		}
+		vDescription += "涂布" + vData.coating_time + "，";
+		vDescription += "光照" + vData.illumination_time;
+		vDescription += NewLine;
+
+		vDescription += "7. ";
+		vDescription += "树脂：" + vData.resin + "，";
+		vDescription += "颜色：" + vData.color_of_resin;
+		vDescription += NewLine;
+
+		vDescription += "8. " + vData.modification + "，";
+		vDescription += vData.lamp + "光照" + vData.time_of_lamp;
+		vDescription += NewLine;
+
+		vDescription += "9. ";
+		vDescription += vData.modulo + "调合" + "，";
+		vDescription += vData.polishing + "抛光";
+
+		return vDescription;
+	}
+	// 美容修复
+	function showLook1(vData, vDescription) {
+		vDescription += "1. ";
+		// FIXME: add tooth location
+		if (vData.anesthesia_medicine != "无") {
+			vDescription += vData.anesthesia_medicine + "，";
+		}
+		if (vData.part_anesthesia != "无") {
+			vDescription += vData.part_anesthesia + "，";
+		}
+		vDescription += vData.rubber + "，";
+		vDescription += vData.microscope + "，";
+		vDescription += vData.tools;
+		vDescription += NewLine;
+
+		vDescription += "2. ";
+		// FIXME: add tooth location
+		if (vData.depth_of_hole != "否") {
+			vDescription += vData.depth_of_hole;
+
+			if (vData.shape_of_hole != "无") {
+				vDescription += "，" + vData.shape_of_hole + "保护牙龈";
+			}
+			if (vData.is_piece != "无") {
+				vDescription += "，低速手机装上" + vData.is_piece;
+			}
+			if (vData.is_chock != "无") {
+				vDescription += "，" + vData.is_chock + "微研磨剂打磨";
+			}
+			if (vData.shade_guide != "无") {
+				vDescription += "，冲洗，吹干，涂布" + vData.shade_guide + "氟保护剂";
+			}
+			vDescription += "。";
+		}
+		vDescription += "酸蚀剂酸蚀病变区域及周围牙体组织" + vData.coating_time + "，";
+		vDescription += "涂布干燥剂" + vData.resin + "，";
+		vDescription += "涂布渗透树脂，光固化" + vData.illumination_time;
+	
+		return vDescription;
+	}
+	function showLook2(vData, vDescription) {
+		vDescription += "1. ";
+		// FIXME: add tooth location
+		if (vData.anesthesia_medicine != "无") {
+			vDescription += vData.anesthesia_medicine + "，";
+		}
+		if (vData.part_anesthesia != "无") {
+			vDescription += vData.part_anesthesia + "，";
+		}
+		vDescription += vData.rubber;
+		vDescription += NewLine;
+
+		vDescription += "2. ";
+		// FIXME: add tooth location
+		if (vData.microscope == "显微镜下") {
+			vDescription += vData.microscope + "，";
+		}
+		vDescription += vData.tools + "去龋，以龋蚀显示剂指示，继续去净龋坏，";
+		vDescription += vData.shape_of_hole + "制备洞形，深度：";
+		vDescription += vData.depth_of_hole + "mm" + NewLine;
+
+		vDescription += "3. 干燥，隔湿，";
+		vDescription += vData.is_piece + "，";
+		vDescription += vData.is_chock + NewLine;
+
+		vDescription += "4. ";
+		vDescription += vData.shade_guide + "比色，选择牙色";
+		vDescription += vData.color_of_tooth + NewLine;
+
+		vDescription += "5. ";
+		vDescription += vData.disinfect + "窝洞消毒";
+		if (vData.bottom != "") {
+			vDescription += "，" + vData.bottom;
+		}
+		vDescription += NewLine;
+
+		vDescription += "6. ";
+		vDescription += vData.etching_type + "：";
+		if (vData.etching_type == "全酸蚀粘接系统") {
+			vDescription += vData.full_etching + "，";
+		} else {
+			vDescription += vData.self_etching + "，";
+		}
+		vDescription += "涂布" + vData.coating_time + "，";
+		vDescription += "光照" + vData.illumination_time;
+		vDescription += NewLine;
+
+		vDescription += "7. ";
+		vDescription += "树脂：" + vData.resin + "，";
+		vDescription += "颜色：" + vData.color_of_resin;
+		vDescription += NewLine;
+
+		vDescription += "8. " + vData.modification + "，";
+		vDescription += vData.lamp + "光照" + vData.time_of_lamp;
+		vDescription += NewLine;
+
+		vDescription += "9. ";
+		vDescription += vData.modulo + "调合" + "，";
+		vDescription += vData.polishing + "抛光";
+		
+		return vDescription;
+	}
+	// 间接修复
+	function showIndirect(vData, vDescription) {
+
+		
+		
 		return vDescription;
 	}
 	// Handle error
